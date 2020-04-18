ad622e803c7b7458172999bed9d8f2620c89579e
Pier-Hugo <pierhugocarpentier36@gmail.com>
2020-04-18 19:25:52 +0200

    Model : getters & setters

--- src/main/java/model/Destination.java
+++ src/main/java/model/Destination.java
@@ -5,4 +5,34 @@ public class Destination
     private String information;
     private String visits;
     private String activities;
+
+    public String getInformation()
+    {
+        return information;
+    }
+
+    public void setInformation(String information)
+    {
+        this.information = information;
+    }
+
+    public String getVisits()
+    {
+        return visits;
+    }
+
+    public void setVisits(String visits)
+    {
+        this.visits = visits;
+    }
+
+    public String getActivities()
+    {
+        return activities;
+    }
+
+    public void setActivities(String activities)
+    {
+        this.activities = activities;
+    }
 }
--- src/main/java/model/Offer.java
+++ src/main/java/model/Offer.java
@@ -6,4 +6,44 @@ public class Offer
     private String description;
     private float price;
     private float discount;
+
+    public String getDestination()
+    {
+        return destination;
+    }
+
+    public void setDestination(String destination)
+    {
+        this.destination = destination;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public float getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(float price)
+    {
+        this.price = price;
+    }
+
+    public float getDiscount()
+    {
+        return discount;
+    }
+
+    public void setDiscount(float discount)
+    {
+        this.discount = discount;
+    }
 }
