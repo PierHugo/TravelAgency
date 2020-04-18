a8271c595a0e852e6fe0a97b87b8bac7aa56a8a0
Pier-Hugo <pierhugocarpentier36@gmail.com>
2020-04-18 19:23:00 +0200

    Model : création

---
+++ src/main/java/model/Destination.java
@@ -0,0 +1,8 @@
+package main.java.model;
+
+public class Destination
+{
+    private String information;
+    private String visits;
+    private String activities;
+}
---
+++ src/main/java/model/Offer.java
@@ -0,0 +1,9 @@
+package main.java.model;
+
+public class Offer
+{
+    private String destination;
+    private String description;
+    private float price;
+    private float discount;
+}
